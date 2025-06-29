867461b0bbde5cef00a7a4af37703aa59a74b630
SamyakBhilwara <ncrgujju@gmail.com>
2025-06-29 23:17:09 +0530

    Delete pages directory

--- pages/admin.js
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>Admin Panel</h1>
-      <p>Login coming soon.</p>
-    </main>
-  );
-}
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>Bhilwara Computer Hub</h1>
-      <p>Welcome to our institute. Learn professional IT skills today!</p>
-      <a href="https://wa.me/917413914777" target="_blank">Chat on WhatsApp</a>
-    </main>
-  );
-}
