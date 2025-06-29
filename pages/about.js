756c201322e3ec5e7d3ed3b40d495403510b83ea
SamyakBhilwara <ncrgujju@gmail.com>
2025-06-29 23:16:28 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,1 @@
-export default function About() {
-  return (
-    <main style={{ padding: '2rem' }}>
-      <h1>About Us</h1>
-      <p>We provide industry-level IT education at Bhilwara Computer Hub.</p>
-    </main>
-  );
-}
+
