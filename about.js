9f4fda9217eded74f838b5779cb1ce49253b9e88
SamyakBhilwara <ncrgujju@gmail.com>
2025-06-29 22:55:32 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>About Us</h1>
+      <p>We provide industry-level IT education at Bhilwara Computer Hub.</p>
+    </main>
+  );
+}
---
+++ admin.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Admin Panel</h1>
+      <p>Login coming soon.</p>
+    </main>
+  );
+}
